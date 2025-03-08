bd9f3922d54a1fea7b1de37cf10ca328f9d70bab
nattida-sine <nattida.pua@hlabconsulting.com>
2025-03-08 16:05:12 +0700

    implement ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import icBack from "./ic-back.svg"
+import icClose from "./ic-close.svg"
+import icCongratulation from "./ic-congratulation.svg"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <header>
+                <img src={icBack} alt="Back" className="back"/>
+                <h1>สินเชื่อ เพย์ เน็กซ์ เอ็กซ์ตร้า</h1>
+                <img src={icClose} alt="Close" className="close"/>
+            </header>
+            <div className="content">
+                <img src={icCongratulation} alt="Congratulations" className="img-congratulations"/>
+                <h2>ยินดีด้วย!</h2>
+                <p>สินเชื่อ เพย์ เน็กซ์ เอ็กซ์ตร้า ของคุณ <br/>ได้รับการอนุมัติแล้ว</p>
+                <div className="info-box">
+                    <span>วงเงินสินเชื่อที่ได้รับอนุมัติ</span>
+                    <span className="amount">฿ 50,000.00</span>
+                </div>
+                <p className="note">แจ้งบริการหักชำระสินเชื่ออัตโนมัติ<br/>
+                    บริษัทจะดำเนินการหักชำระหนี้ทั้งหมดผ่านบัญชีทรูมันนี่/ช่องทางอื่นๆที่ผูกไว้กับทรูมันนี้ กรณีผิดนัดชำระสินเชื่อเท่านั้น</p>
+            </div>
+            <button className="action-button">เริ่มใช้งานเพย์ เน็กซ์ เอ็กซ์ตร้า</button>
+        </div>
+    );
 }
 
 export default App;
