c02096edc8f7128900c3481795ed1f9febdbaad7
nattida-sine <nattida.pua@hlabconsulting.com>
2025-03-09 14:54:38 +0700

    update App unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByTestId('header-text');
+  const headerContentElement = screen.getByTestId('header-content');
+  const descriptionElement = screen.getByTestId('description');
+  const approvalInfoDetailsElement = screen.getByTestId('approval-info-details');
+  const termsConditionsElement = screen.getByTestId('terms-conditions');
+  const actionButtonElement = screen.getByTestId('action-button');
+
+  expect(headerElement).toBeInTheDocument();
+  expect(headerContentElement).toBeInTheDocument();
+  expect(descriptionElement).toBeInTheDocument();
+  expect(approvalInfoDetailsElement).toBeInTheDocument();
+  expect(termsConditionsElement).toBeInTheDocument();
+  expect(actionButtonElement).toBeInTheDocument();
 });
